4ea384a6777a9fea16adcc0ee5c1639c1a6c7a04
Vinicius Alves <vinialves.dev@gmail.com>
2026-08-10 19:40:15 -0300

    ajuste na configuração de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  constructor(private configService: ConfigService) {}
-}
+export class AppModule {}
