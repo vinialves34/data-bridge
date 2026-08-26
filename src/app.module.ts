e62f636346644e8b91de5c0ca6938d6cdc75c38f
Vinicius Alves <vinialves.dev@gmail.com>
2026-08-26 19:15:08 -0300

    refatoração das funções do playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,22 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabasePlaygroundService } from './database/database-playground.service';
 import { PostgresConnectionProvider } from './migration/infrastructure/database/postgres/postgres-connection.provider';
 import { PostgresSourceAdapter } from './migration/infrastructure/database/postgres/postgres-source.adapter';
+import { validate } from 'shared/config/environment/env.validation';
+import { MongoDestinationAdapter } from './migration/infrastructure/database/mongodb/mongodb-destination.adapter';
+import { MongoConnectionProvider } from './migration/infrastructure/database/mongodb/mongodb-connection.provider';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validate,
     }),
   ],
   providers: [
     PostgresConnectionProvider,
     PostgresSourceAdapter,
+    MongoConnectionProvider,
+    MongoDestinationAdapter,
     DatabasePlaygroundService,
   ],
 })
